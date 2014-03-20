276c2b941599e6bafdf9f2efa2159fa0134eaaf3
fabiofumarola <fabiofumarola@gmail.com>
2014-03-20 12:10:15 +0100

    add base test class

---
+++ src/test/scala/it/dtk/nlp/BaseTestClass.scala
@@ -0,0 +1,9 @@
+package it.dtk.nlp
+
+import org.scalatest._
+
+/**
+ * @author fabiofumarola
+ * we must read the documentation at http://www.scalatest.org/user_guide/defining_base_classes and the consecutive pages
+ */
+class BaseTestClass extends WordSpec with Matchers with OptionValues
