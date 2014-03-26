5c5dc4972b2ef9a71c30589251c8a6a1b5acf0fa
Michele Damiano Torelli <me@mdtorelli.it>
2014-03-26 11:31:51 +0100

    Add trait class for detectors

---
+++ src/main/scala/it/dtk/nlp/detector/Detector.scala
@@ -0,0 +1,15 @@
+package it.dtk.nlp.detector
+
+import it.dtk.nlp.Word
+
+/**
+ * Entry point for detector classes
+ *
+ * @author Michele Damiano Torelli <daniele@datatoknowledge.it>
+ *
+ */
+trait Detector {
+
+  def detect(words: Seq[Word]): Vector[String]
+
+}
