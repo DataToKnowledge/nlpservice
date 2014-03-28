987d119382398bc15fef745a56f8b1f755b297c9
Andrea Scarpino <andrea@datatoknowledge.it>
2014-03-28 14:33:46 +0100

    Update Models and add news ones

--- src/main/scala/it/dtk/nlp/Model.scala
+++ src/main/scala/it/dtk/nlp/Model.scala
@@ -1,31 +1,37 @@
 package it.dtk.nlp
 
 import org.joda.time.DateTime
-import java.util.Date
 
 /**
  * @author Andrea Scarpino <andrea@datatoknowledge.it>
  */
 
 case class Word(token: String, tokenId: Option[Int] = None, tokenStart: Option[Int] = None, tokenEnd: Option[Int] = None,
-    sentence: Option[String] = None, posTag: Option[String] = None, lemma: Option[String] = None, compMorpho: Option[String] = None,
-    stem: Option[String] = None, iobEntity: Option[Seq[String]] = None , chunk: Option[String] = None)
-    
+                sentence: Option[String] = None, posTag: Option[String] = None, lemma: Option[String] = None, compMorpho: Option[String] = None,
+                stem: Option[String] = None, iobEntity: Option[Seq[String]] = None, chunk: Option[String] = None)
 
-case class Sentence(words: Seq[Word])
+case class Sentence(words: Seq[Word] = Vector.empty)
 
-case class NLPTitle(sentences: Seq[Sentence])
+case class NLPTitle(sentences: Seq[Sentence] = Vector.empty)
 
-case class NLPSummary(sentences: Seq[Sentence])
+case class NLPSummary(sentences: Seq[Sentence] = Vector.empty)
 
-case class NLPText(sentences: Seq[Sentence])
+case class NLPText(sentences: Seq[Sentence] = Vector.empty)
 
-case class News(id: Option[String] = None, urlWebSite: Option[String], urlNews: Option[String],
-                title: Option[String], summary: Option[String], newsDate: Option[DateTime],
-                text: Option[String] = None, tags: Option[Set[String]] = None,
-                metaDescription: Option[String] = None, metaKeyword: Option[String] = None,
-                canonicalUrl: Option[String] = None, topImage: Option[String] = None, 
-                nlpTitle: Option[NLPTitle] = None, nlpSummary: Option[NLPSummary] = None, nlpText: Option[NLPText] = None,
-                crime: Option[Seq[String]] = None, address: Option[Seq[String]] = None,  person: Option[Seq[String]] = None,  
-                location: Option[Seq[String]] = None, date: Option[Seq[String]] = None, organization: Option[Seq[String]] = None,
-                nlpTags: Option[Map[String,Double]] = None)
+case class News(id: String, urlWebSite: String, urlNews: String, title: Option[String], summary: Option[String],
+                newsDate: Option[DateTime], text: Option[String] = None, tags: Option[Set[String]] = None,
+                metaDescription: Option[String] = None, metaKeyword: Option[String] = None, canonicalUrl: Option[String] = None,
+                topImage: Option[String] = None, nlpTitle: Option[NLPTitle] = None, nlpSummary: Option[NLPSummary] = None,
+                nlpText: Option[NLPText] = None, crime: Option[Seq[String]] = None, address: Option[Seq[String]] = None,
+                person: Option[Seq[String]] = None, location: Option[Seq[String]] = None, date: Option[Seq[String]] = None,
+                organization: Option[Seq[String]] = None, nlpTags: Option[Map[String, Double]] = None)
+
+case class Lemma(id: String, word: String, lemma: Option[String] = None, features: Option[String] = None)
+
+case class City(id: String, city_name: String, cap: Option[String], province: Option[String], province_code: Option[String],
+                region: Option[String], region_code: Option[String], state: Option[String])
+
+case class Crime(id: String, word: String, lemma: Option[String] = None, stem: Option[String] = None, tipo: Option[String] = None)
+
+case class Address(id: String, street: String, cap: Option[String] = None, city: Option[String] = None, province: Option[String] = None,
+                   state: Option[String] = None, region: Option[String] = None)
--- src/main/scala/it/dtk/nlp/MongoDBMapper.scala
+++ src/main/scala/it/dtk/nlp/MongoDBMapper.scala
@@ -1,25 +1,26 @@
 package it.dtk.nlp
 
 import com.mongodb.casbah.Imports._
-import com.mongodb.DBObject
 import com.mongodb.casbah.commons.conversions.scala._
 import org.joda.time.DateTime
 
 object MongoDBMapper {
 
-  def wordToDBO(w: Word): MongoDBObject =
+  def wordToDBO(word: Word): MongoDBObject = {
     MongoDBObject(
-      "token" -> w.token,
-      "tokenId" -> w.tokenId.get,
-      "tokenStart" -> w.tokenStart.get,
-      "tokenEnd" -> w.tokenStart.get,
-      "sentence" -> w.sentence.get,
-      "posTag" -> w.posTag.get,
-      "lemma" -> w.lemma.get,
-      "com    Morpho" -> w.compMorpho.get,
-      "stem" -> w.stem.get,
-      "iobEntity" -> w.iobEntity.getOrElse(Vector.empty),
-      "chunk" -> w.chunk)
+      "token" -> word.token,
+      "tokenId" -> word.tokenId,
+      "tokenStart" -> word.tokenStart,
+      "tokenEnd" -> word.tokenStart,
+      "sentence" -> word.sentence,
+      "posTag" -> word.posTag,
+      "lemma" -> word.lemma,
+      "com    Morpho" -> word.compMorpho,
+      "stem" -> word.stem,
+      "iobEntity" -> word.iobEntity.getOrElse(Vector.empty),
+      "chunk" -> word.chunk
+    )
+  }
 
   def dBOToWord(dbo: MongoDBObject): Word = {
     Word(
@@ -36,68 +37,163 @@ object MongoDBMapper {
       dbo.getAs[String]("chunk"))
   }
 
-  def sentenceToDBO(s: Sentence): MongoDBObject = {
+  def sentenceToDBO(sentence: Sentence): MongoDBObject = {
     MongoDBObject(
-      "words" -> s.words.map(wordToDBO(_)))
+      "words" -> sentence.words.map(wordToDBO)
+    )
   }
 
   def dBOToSentence(dbo: MongoDBObject): Sentence = {
     val seqDBO = dbo.getAs[Seq[MongoDBObject]]("words")
     //convert to words
-    val seqWords = seqDBO.map(s => s.map(dBOToWord(_)))
-    Sentence(seqWords.getOrElse(Vector.empty[Word]))
+    val seqWords = seqDBO.map(s => s.map(dBOToWord))
+    Sentence(seqWords.getOrElse(Vector.empty))
   }
 
   def nlpTitleToDBO(title: NLPTitle): MongoDBObject = {
     MongoDBObject(
-      "sentences" -> title.sentences.map(s => sentenceToDBO(s)))
+      "sentences" -> title.sentences.map(s => sentenceToDBO(s))
+    )
   }
 
   def dBOtoNlpTitle(dbo: MongoDBObject): NLPTitle = {
     val seqDBO = dbo.getAs[Seq[MongoDBObject]]("sentences")
     //convert to sentences
-    val seqSentences = seqDBO.map(s => s.map(dBOToSentence(_)))
-    NLPTitle(seqSentences.getOrElse(Vector.empty[Sentence]))
+    val seqSentences = seqDBO.map(s => s.map(dBOToSentence))
+    NLPTitle(seqSentences.getOrElse(Vector.empty))
   }
 
-  def nlpSummaryToDBO(title: NLPTitle): MongoDBObject = {
+  def nlpSummaryToDBO(summary: NLPSummary): MongoDBObject = {
     MongoDBObject(
-      "sentences" -> title.sentences.map(s => sentenceToDBO(s)))
+      "sentences" -> summary.sentences.map(s => sentenceToDBO(s))
+    )
   }
 
-  def dBOtoNlpSummary(dbo: MongoDBObject): NLPTitle = {
+  def dBOtoNlpSummary(dbo: MongoDBObject): NLPSummary = {
     val seqDBO = dbo.getAs[Seq[MongoDBObject]]("sentences")
     //convert to sentences
-    val seqSentences = seqDBO.map(s => s.map(dBOToSentence(_)))
-    NLPTitle(seqSentences.getOrElse(Vector.empty[Sentence]))
+    val seqSentences = seqDBO.map(s => s.map(dBOToSentence))
+    NLPSummary(seqSentences.getOrElse(Vector.empty))
   }
 
-  def nlpTextToDBO(title: NLPTitle): MongoDBObject = {
-    MongoDBObject("sentences" -> title.sentences.map(s => sentenceToDBO(s)))
+  def nlpTextToDBO(text: NLPText): MongoDBObject = {
+    MongoDBObject(
+      "sentences" -> text.sentences.map(s => sentenceToDBO(s))
+    )
   }
 
-  def dBOtoNlpText(dbo: MongoDBObject): NLPTitle = {
+  def dBOtoNlpText(dbo: MongoDBObject): NLPText = {
     val seqDBO = dbo.getAs[Seq[MongoDBObject]]("sentences")
     //convert to sentences
-    val seqSentences = seqDBO.map(s => s.map(dBOToSentence(_)))
-    NLPTitle(seqSentences.getOrElse(Vector.empty[Sentence]))
+    val seqSentences = seqDBO.map(s => s.map(dBOToSentence))
+    NLPText(seqSentences.getOrElse(Vector.empty))
   }
-  
+
   def dBOToNews(dbo: MongoDBObject): News = {
     RegisterJodaTimeConversionHelpers()
     News(
-     dbo._id.map(_.toString),
-     dbo.getAs[String]("urlWebSite"),
-     dbo.getAs[String]("urlNews"),
-     dbo.getAs[String]("title"),
-     dbo.getAs[String]("summary"),
-     dbo.getAs[DateTime]("newsDate"),
-     dbo.getAs[String]("text"),
-     dbo.getAs[Set[String]]("tags"),
-     dbo.getAs[String]("metaDescription"),
-     dbo.getAs[String]("metaKeyword"),
-     dbo.getAs[String]("canonicalUrl"),
-     dbo.getAs[String]("topImage")
+      dbo._id.map(_.toString).get,
+      dbo.getAs[String]("urlWebSite").get,
+      dbo.getAs[String]("urlNews").get,
+      dbo.getAs[String]("title"),
+      dbo.getAs[String]("summary"),
+      dbo.getAs[DateTime]("newsDate"),
+      dbo.getAs[String]("text"),
+      dbo.getAs[Set[String]]("tags"),
+      dbo.getAs[String]("metaDescription"),
+      dbo.getAs[String]("metaKeyword"),
+      dbo.getAs[String]("canonicalUrl"),
+      dbo.getAs[String]("topImage")
     )
   }
+
+  def NewsToDBO(news: News): MongoDBObject = {
+    MongoDBObject(
+      "urlWebSite" -> news.urlWebSite,
+      "urlNews" -> news.urlNews,
+      "title" -> news.title,
+      "summary" -> news.summary,
+      "newsDate" -> news.newsDate,
+      "text" -> news.text,
+      "tags" -> news.tags,
+      "metaDescription" -> news.metaDescription,
+      "metaKeyword" -> news.metaKeyword,
+      "canonicalUrl" -> news.canonicalUrl,
+      "topImage" -> news.topImage
+    )
+  }
+
+  def dBOtoLemma(dbo: MongoDBObject): Lemma = {
+    Lemma(
+      dbo._id.map(_.toString).get,
+      dbo.getAs[String]("word").get,
+      dbo.getAs[String]("lemma"),
+      dbo.getAs[String]("features")
+    )
+  }
+
+  def lemmaToDBO(lemma: Lemma): MongoDBObject = {
+    MongoDBObject(
+      "word" -> lemma.word,
+      "lemma" -> lemma.lemma,
+      "features" -> lemma.features
+    )
+  }
+
+  def dBOtoCity(dbo: MongoDBObject): City = {
+    City(
+      dbo._id.map(_.toString).get,
+      dbo.getAs[String]("city_name").get,
+      dbo.getAs[String]("cap"),
+      dbo.getAs[String]("province"),
+      dbo.getAs[String]("province_code"),
+      dbo.getAs[String]("region"),
+      dbo.getAs[String]("region_code"),
+      dbo.getAs[String]("state")
+    )
+  }
+
+  def cityToDBO(city: City): MongoDBObject = {
+    MongoDBObject(
+      "city_name" -> city.city_name,
+      "cap" -> city.cap,
+      "province" -> city.province,
+      "province_code" -> city.province_code,
+      "region" -> city.region,
+      "region_code" -> city.region_code,
+      "state" -> city.state
+    )
+  }
+
+  def dBOtoCrime(dbo: MongoDBObject): Crime = {
+    Crime(
+      dbo._id.map(_.toString).get,
+      dbo.getAs[String]("word").get,
+      dbo.getAs[String]("lemma"),
+      dbo.getAs[String]("stem"),
+      dbo.getAs[String]("tipo")
+    )
+  }
+
+  def crimeToDBO(crime: Crime): MongoDBObject = {
+    MongoDBObject(
+      "word" -> crime.word,
+      "lemma" -> crime.lemma,
+      "stem" -> crime.stem,
+      "type" -> crime.tipo
+    )
+  }
+
+  def dBOtoAddress(dbo: MongoDBObject): Address = {
+    Address(
+      dbo._id.map(_.toString).get,
+      dbo.getAs[String]("street").get,
+      dbo.getAs[String]("cap"),
+      dbo.getAs[String]("city"),
+      dbo.getAs[String]("province"),
+      dbo.getAs[String]("state"),
+      dbo.getAs[String]("region")
+    )
+  }
+
 }
