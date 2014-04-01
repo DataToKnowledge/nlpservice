dc7cdc5b0ccfc26cd365842fb5863949426ba1c4
Fabiana Lanotte <fabiana.lan@hotmail.it>
2014-04-01 11:07:54 +0200

    add crime detector

---
+++ src/main/scala/it/dtk/nlp/detector/CrimeDetectorGVE.scala
@@ -0,0 +1,70 @@
+package it.dtk.nlp.detector
+
+import it.dtk.nlp.db.Word
+import it.dtk.nlp.db.DBManager
+import it.dtk.nlp.db.Crime
+import it.dtk.nlp.db.Sentence
+import scala.util.control.Breaks._
+object CrimeDetectorGVE {
+  var crimeWords: List[String] = List()
+  var result = Vector.empty[Word]
+  val offset = 3
+}
+class CrimeDetectorGVE extends Detector {
+  import CrimeDetectorGVE._
+  /**
+   * load crime dictionary from file
+   */
+  private def downloadCrimeDictionary(): Unit = {
+    val dictionary = getClass().getResource("/CrimeDictionary").getPath()
+    val lines = scala.io.Source.fromFile(dictionary).getLines.drop(0).map(_.split(","))
+
+    lines.foreach { a =>
+      (a(0).trim() :: crimeWords)
+    }
+
+  }
+  private def getString(list: Seq[Word]): String = {
+    list.map(elem => elem.token.toLowerCase()).mkString(" ")
+  }
+
+  private def setEntity(sentence: Sentence, start: Int, end: Int): Unit = {
+    var currentWord = sentence.words.apply(start)
+    result :+= currentWord.copy(iobEntity = currentWord.iobEntity :+ "B-CRIME")
+    for (i <- (start + 1) to end) {
+      currentWord = sentence.words.apply(i)
+      result :+= currentWord.copy(iobEntity = currentWord.iobEntity :+ "I-CRIME")
+    }
+  }
+  override def detect(sentence: Sentence): Sentence = {
+    var candidates = Seq()
+    var i = 0
+    while (i < sentence.words.size) {
+      val subSeq = sentence.words.slice(i, i + offset)
+      var entityBool = false
+      for (j <- offset to 1 by -1) {
+        var candidate = subSeq.slice(0, j)
+        DBManager.findCrime(getString(candidate).trim) match {
+          case Some(token: Crime) =>
+            //setting crimeEntity only when the end is less then the sentence size
+            var end = i + j - 1
+            if (end < sentence.words.size) {
+              setEntity(sentence, i, end)
+              entityBool = true
+              i = i + j
+            }
+          case None =>
+            None
+
+        }
+      }
+      //una volta calcolate tutte le possibili combinazioni si sceglie la migliore per quella slice
+      if (entityBool == false) {
+        result :+= sentence.words(i)
+        i = i + 1
+      }
+
+    }
+    Sentence(result)
+  }
+}
