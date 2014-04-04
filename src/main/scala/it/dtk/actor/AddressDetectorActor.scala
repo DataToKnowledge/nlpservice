e0674a141ddc903ce63ef327aec63288b370fbe0
Andrea Scarpino <andrea@datatoknowledge.it>
2014-04-04 11:44:50 +0000

    Implement actors detectors' receive methods

--- src/main/scala/it/dtk/actor/AddressDetectorActor.scala
+++ src/main/scala/it/dtk/actor/AddressDetectorActor.scala
@@ -2,9 +2,34 @@ package it.dtk.actor
 
 import akka.actor.Actor
 import akka.actor.ActorLogging
+import it.dtk.actor.NewsPart._
+import it.dtk.nlp.db.Sentence
+import it.dtk.nlp.detector.AddressDetector
 
+object AddressDetectorActor {
+  case class Process(newsId: String, sentences: Seq[Sentence], value: NewsPart)
+  case class Result(newsId: String, sentences: Seq[Sentence], value: NewsPart)
+}
+
+/**
+ *
+ * @author Andrea Scarpino <andrea@datatoknowledge.it>
+ */
 class AddressDetectorActor extends Actor with ActorLogging {
 
-  def receive = ???
+  import AddressDetectorActor._
+
+  def receive = {
+
+    case Process(newsId, sentences, Title) =>
+      sender() ! Result(newsId, sentences.map(AddressDetector.detect), Title)
+
+    case Process(newsId, sentences, Summary) =>
+      sender() ! Result(newsId, sentences.map(AddressDetector.detect), Summary)
+
+    case Process(newsId, sentences, Corpus) =>
+      sender() ! Result(newsId, sentences.map(AddressDetector.detect), Corpus)
+
+  }
   
 }
--- src/main/scala/it/dtk/actor/CityDetectorActor.scala
+++ src/main/scala/it/dtk/actor/CityDetectorActor.scala
@@ -1,9 +1,34 @@
 package it.dtk.actor
 
 import akka.actor.{Actor, ActorLogging}
+import it.dtk.nlp.db.Sentence
+import it.dtk.actor.NewsPart._
+import it.dtk.nlp.detector.CityDetector
 
+object CityDetectorActor {
+  case class Process(newsId: String, sentences: Seq[Sentence], value: NewsPart)
+  case class Result(newsId: String, sentences: Seq[Sentence], value: NewsPart)
+}
+
+/**
+ *
+ * @author Andrea Scarpino <andrea@datatoknowledge.it>
+ */
 class CityDetectorActor extends Actor with ActorLogging {
 
-  def receive = ???
-  
+  import CityDetectorActor._
+
+  def receive = {
+
+    case Process(newsId, sentences, Title) =>
+      sender() ! Result(newsId, sentences.map(CityDetector.detect), Title)
+
+    case Process(newsId, sentences, Summary) =>
+      sender() ! Result(newsId, sentences.map(CityDetector.detect), Summary)
+
+    case Process(newsId, sentences, Corpus) =>
+      sender() ! Result(newsId, sentences.map(CityDetector.detect), Corpus)
+
+  }
+
 }
