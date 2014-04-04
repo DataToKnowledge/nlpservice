88063dcef19c825784df900678654e17b1b01329
Andrea Scarpino <andrea@datatoknowledge.it>
2014-04-04 12:35:20 +0000

    Implements SentenceDetectorActor and TokenizerActor

--- src/main/scala/it/dtk/actor/SentenceDetectorActor.scala
+++ src/main/scala/it/dtk/actor/SentenceDetectorActor.scala
@@ -1,9 +1,33 @@
 package it.dtk.actor
 
 import akka.actor.{Actor, ActorLogging}
+import it.dtk.actor.NewsPart._
+import it.dtk.nlp.TextPreprocessor
 
+object SentenceDetectorActor {
+  case class Process(newsId: String, text: String, value: NewsPart)
+  case class Result(newsId: String, sentences: Seq[String], value: NewsPart)
+}
+
+/**
+ *
+ * @author Andrea Scarpino <andrea@datatoknowledge.it>
+ */
 class SentenceDetectorActor extends Actor with ActorLogging {
 
-  def receive = ???
+  import SentenceDetectorActor._
+
+  def receive = {
+
+    case Process(newsId, text, Title) =>
+      sender() ! Result(newsId, TextPreprocessor.getSentences(text), Title)
+
+    case Process(newsId, text, Summary) =>
+      sender() ! Result(newsId, TextPreprocessor.getSentences(text), Summary)
+
+    case Process(newsId, text, Corpus) =>
+      sender() ! Result(newsId, TextPreprocessor.getSentences(text), Corpus)
+
+  }
   
 }
--- src/main/scala/it/dtk/actor/TokenizerActor.scala
+++ src/main/scala/it/dtk/actor/TokenizerActor.scala
@@ -1,9 +1,34 @@
 package it.dtk.actor
 
 import akka.actor.{Actor, ActorLogging}
+import it.dtk.actor.NewsPart._
+import it.dtk.nlp.TextPreprocessor
+import it.dtk.nlp.db.Sentence
 
+object TokenizerActor {
+  case class Process(newsId: String, text: String, value: NewsPart)
+  case class Result(newsId: String, sentence: Sentence, value: NewsPart)
+}
+
+/**
+ *
+ * @author Andrea Scarpino <andrea@datatoknowledge.it>
+ */
 class TokenizerActor extends Actor with ActorLogging {
 
-  def receive = ???
+  import TokenizerActor._
+
+  def receive = {
+
+    case Process(newsId, text, Title) =>
+      sender() ! Result(newsId, TextPreprocessor.getTokens(text), Title)
+
+    case Process(newsId, text, Summary) =>
+      sender() ! Result(newsId, TextPreprocessor.getTokens(text), Summary)
+
+    case Process(newsId, text, Corpus) =>
+      sender() ! Result(newsId, TextPreprocessor.getTokens(text), Corpus)
+
+  }
   
 }
