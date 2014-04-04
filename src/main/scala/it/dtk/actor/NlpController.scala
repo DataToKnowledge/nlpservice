b550b28a86bcb177decb2d97e48c58c63042f820
fabiofumarola <fabiofumarola@gmail.com>
2014-04-04 14:01:09 +0200

    setup NlpController and TextProActor

--- src/main/scala/it/dtk/actor/NlpController.scala
+++ src/main/scala/it/dtk/actor/NlpController.scala
@@ -3,6 +3,10 @@ package it.dtk.actor
 import akka.actor.{ Actor, ActorLogging }
 import it.dtk.nlp.db.News
 import akka.actor.ActorRef
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import java.util.concurrent.Executor
+import scala.concurrent.ExecutionContext
 
 object NlpController {
 
@@ -18,11 +22,14 @@ object NewsPart extends Enumeration {
 
 class NlpController extends Actor with ActorLogging {
 
+  implicit val exec = context.dispatcher.asInstanceOf[Executor with ExecutionContext]
+
   import NlpController._
   import NewsPart._
-  
-  //private var mapNews = Map.empty[String,News]
-  
+  import TextProActor._
+
+  val callInterval = 10 seconds
+
   def receive = waiting
 
   val waiting: Receive = {
@@ -30,20 +37,46 @@ class NlpController extends Actor with ActorLogging {
       log.debug("start processing {} news", newsSeq.length)
       context.become(runNext(newsSeq, sender))
   }
-  
+
   def runNext(newsSeq: Seq[News], send: ActorRef): Receive = {
-      val setIds = newsSeq.map(_.id).toSet
-      val mapNews = newsSeq.foldLeft(Map.empty[String,News])((map, news) => map + (news.id -> news)) 
-      val send = sender
-      //start pipeline
-      newsSeq.foreach { n =>
-        
+    val setIds = newsSeq.map(_.id).toSet
+    val mapNews = newsSeq.foldLeft(Map.empty[String, News])((map, news) => map + (news.id -> news))
+    val send = sender
+    //start pipeline
+    //TODO now we use the sceduler, but we can define also a consumer policy
+    newsSeq.foreach { n =>
+      val textProActor = context.actorOf(TextProActor.props, "textPro_id" + n.id)
+      var interval = callInterval
+      n.title.map { title =>
+        context.system.scheduler.scheduleOnce(interval, textProActor, Parse(n.id, title, Title))
+        interval += callInterval
       }
-      running(setIds,mapNews,send)
+      n.summary.map { summary =>
+        context.system.scheduler.scheduleOnce(interval, textProActor, Parse(n.id, summary, Summary))
+        interval += callInterval
+      }
+
+      n.summary.map { text =>
+        context.system.scheduler.scheduleOnce(interval, textProActor, Parse(n.id, text, Corpus))
+        interval += callInterval
+      }
+
+      //nlpActor ! 
+    }
+    running(setIds, mapNews, send)
   }
-  
-  def running(setIds: Set[String], mapNews: Map[String,News], send: ActorRef): Receive = {
-    ???
+
+  def running(setIds: Set[String], mapNews: Map[String, News], send: ActorRef): Receive = {
+
+    case TextProActor.Result(newsId, sentences, keywords, Title) =>
+
+    case TextProActor.Result(newsId, sentences, keywords, Summary) =>
+
+    case TextProActor.Result(newsId, sentences, keywords, Corpus) =>
+
+    case TextProActor.Fail(newsId, text, newsPart) =>
+    //TODO reschedule the message
+
   }
 
 }
--- src/main/scala/it/dtk/actor/TextProActor.scala
+++ src/main/scala/it/dtk/actor/TextProActor.scala
@@ -3,24 +3,37 @@ package it.dtk.actor
 import akka.actor.{ Actor, ActorLogging }
 import it.dtk.nlp.db.Sentence
 import NewsPart._
+import akka.actor.Props
+import it.dtk.nlp.TextProClient
+import scala.util.Success
+import scala.util.Failure
+import java.util.concurrent.Executor
+import scala.concurrent.ExecutionContext
 
 object TextProActor {
-  case class Process(newsId: String, text: String, value: NewsPart)
-  case class Result(newsId: String, sentences: Seq[Sentence], keywords: Map[String, Double], value: NewsPart)
+  case class Parse(newsId: String, text: String, value: NewsPart)
+  case class Result(newsId: String, sentences: Seq[Sentence], keywords: Map[String, Double], newsPart: NewsPart)
+  case class Fail(newsId: String, text: String, value: NewsPart)
+  
+  def props = Props(classOf[TextProActor])
 }
 
 class TextProActor extends Actor with ActorLogging {
 
+  implicit val exec = context.dispatcher.asInstanceOf[Executor with ExecutionContext]
   import TextProActor._
+
+  private val textProClient = new TextProClient
 
   def receive = {
 
-    case Process(newsId, text, Title) =>
-
-    case Process(newsId, text, Summary) =>
-
-    case Process(newsId, text, Corpus) =>
-
+    case Parse(newsId, text, newsPart) =>
+      val send = sender
+      textProClient.process(text).onComplete {
+        case Success((keywords, sentences)) =>
+          send ! Result(newsId, sentences, keywords, newsPart)
+        case Failure(ex) =>
+          send ! Fail(newsId, text, newsPart)
+      }
   }
-
 }
