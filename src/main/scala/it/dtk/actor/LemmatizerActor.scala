c753810adfd9606b0304ff47950323d8b2408f22
Andrea Scarpino <andrea@datatoknowledge.it>
2014-04-04 12:18:18 +0000

    Implements LemmatizerActor and StemmerActor

--- src/main/scala/it/dtk/actor/LemmatizerActor.scala
+++ src/main/scala/it/dtk/actor/LemmatizerActor.scala
@@ -1,9 +1,32 @@
 package it.dtk.actor
 
 import akka.actor.{Actor, ActorLogging}
+import it.dtk.actor.NewsPart._
+import it.dtk.nlp.db.Sentence
+import it.dtk.nlp.Lemmatizer
 
+object LemmatizerActor {
+  case class Process(newsId: String, sentences: Seq[Sentence], value: NewsPart)
+  case class Result(newsId: String, sentences: Seq[Sentence], value: NewsPart)
+}
+
+/**
+ *
+ * @author Andrea Scarpino <andrea@datatoknowledge.it>
+ */
 class LemmatizerActor extends Actor with ActorLogging {
 
-  def receive = ???
+  import LemmatizerActor._
+
+  def receive = {
+    case Process(newsId, sentences, Title) =>
+      sender() ! Result(newsId, sentences.map(s => Sentence(s.words.map(Lemmatizer.lemma))), Title)
+
+    case Process(newsId, sentences, Summary) =>
+      sender() ! Result(newsId, sentences.map(s => Sentence(s.words.map(Lemmatizer.lemma))), Summary)
+
+    case Process(newsId, sentences, Corpus) =>
+      sender() ! Result(newsId, sentences.map(s => Sentence(s.words.map(Lemmatizer.lemma))), Corpus)
+  }
   
 }
--- src/main/scala/it/dtk/actor/StemmerActor.scala
+++ src/main/scala/it/dtk/actor/StemmerActor.scala
@@ -1,9 +1,33 @@
 package it.dtk.actor
 
 import akka.actor.{Actor, ActorLogging}
+import it.dtk.actor.NewsPart._
+import it.dtk.nlp.db.Sentence
+import it.dtk.nlp.WordStemmer
 
+object StemmerActor {
+  case class Process(newsId: String, sentences: Seq[Sentence], value: NewsPart)
+  case class Result(newsId: String, sentences: Seq[Sentence], value: NewsPart)
+}
+/**
+ *
+ * @author Andrea Scarpino <andrea@datatoknowledge.it>
+ */
 class StemmerActor extends Actor with ActorLogging {
 
-  def receive = ???
+  import StemmerActor._
+
+  def receive = {
+
+    case Process(newsId, sentences, Title) =>
+      sender() ! Result(newsId, sentences.map(s => Sentence(s.words.map(WordStemmer.stem))), Title)
+
+    case Process(newsId, sentences, Summary) =>
+      sender() ! Result(newsId, sentences.map(s => Sentence(s.words.map(WordStemmer.stem))), Summary)
+
+    case Process(newsId, sentences, Corpus) =>
+      sender() ! Result(newsId, sentences.map(s => Sentence(s.words.map(WordStemmer.stem))), Corpus)
+
+  }
   
 }
