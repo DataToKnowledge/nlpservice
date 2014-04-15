c0210c9dc1a43ae11957a91e0caf12688c07d722
Andrea Scarpino <andrea@datatoknowledge.it>
2014-04-15 10:18:00 +0200

    PosTaggerActor: WIP

--- src/main/scala/it/dtk/actor/PosTaggerActor.scala
+++ src/main/scala/it/dtk/actor/PosTaggerActor.scala
@@ -1,10 +1,18 @@
 package it.dtk.actor
 
-import akka.actor.{ Actor, ActorLogging }
+import akka.actor.{Actor, ActorLogging}
+import it.dtk.actor.NewsPart._
 import akka.actor.Props
 import akka.routing.RoundRobinPool
+import it.dtk.nlp.TreeTagger
+import it.dtk.nlp.db.Sentence
+import scala.util.{Failure, Success}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object PosTaggerActor {
+  case class Process(newsId: String, text: Sentence, value: NewsPart)
+  case class Result(newsId: String, sentence: Sentence, value: NewsPart)
+  case class Fail(newsId: String, ex: Throwable, value: NewsPart)
 
   def props = Props(classOf[PosTaggerActor])
 
@@ -16,8 +24,24 @@ object PosTaggerActor {
     RoundRobinPool(nrOfInstances).props(props)
 }
 
+/**
+ *
+ * @author Andrea Scarpino <andrea@datatoknowledge.it>
+ */
 class PosTaggerActor extends Actor with ActorLogging {
 
-  def receive = ???
+  import PosTaggerActor._
 
+  def receive = {
+
+    case Process(newsId, text, Title) =>
+      TreeTagger(text) onComplete {
+        case Success(res) =>
+          sender() ! Result(newsId, res, Title)
+        case Failure(ex) =>
+          sender() ! Fail(newsId, ex, Title)
+      }
+
+  }
+ 
 }
